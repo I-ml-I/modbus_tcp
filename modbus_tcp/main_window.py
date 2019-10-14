2e7d33d0a549381ed39bd94414b9b026751db753
Trainee <trainee@helmholz.local>
2019-10-14 12:53:17 +0200

    created signal and slot for updating ui, output connection status in application insted of console

--- modbus_tcp/main_window.py
+++ modbus_tcp/main_window.py
@@ -3,6 +3,8 @@ from modbus_tcp.utils import network_status
 from modbus_tcp.utils import connectivity
 from modbus_tcp.utils import data_format
 from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QPlainTextEdit
+from PySide2.QtCore import Slot
 
 
 class MainWindow(QMainWindow):
@@ -11,10 +13,16 @@ class MainWindow(QMainWindow):
 
         self.gui = Ui_MainWindow()
         self.gui.setupUi(self)
+        self.showMasterDeviceIp()
 
         self.gui.connect_button.clicked.connect(self.connectButtonClicked)
 
-        self.showMasterDeviceIp()
+        self.ui_elements_refnames = {'connect_button' : self.gui.connect_button,
+                                     'relay_1' : self.gui.relay_1_checkBox,
+                                     'relay_2' : self.gui.relay_2_checkBox,
+                                     'relay_3' : self.gui.relay_3_checkBox,
+                                     'relay_4' : self.gui.relay_4_checkBox,
+                                     'output' : self.gui.communication_inspector_textEdit}
 
     def showMasterDeviceIp(self):
         self.gui.master_ip_lineEdit.setText(network_status.getIpAdress())
@@ -25,6 +33,7 @@ class MainWindow(QMainWindow):
             port = self.gui.port_lineEdit.text()
             
             self.modbus_connection = connectivity.Modbus(coupler_ip, port)
+            self.modbus_connection.communication.updateUI.connect(self.updateUI)
             self.modbus_connection.start()
 
     def inputDataFieldsValid(self):
@@ -39,3 +48,22 @@ class MainWindow(QMainWindow):
         if not port_valid : self.gui.port_lineEdit.setText("non valid port value")
 
         return coupler_ip_valid and port_valid
+
+    @Slot(dict)
+    def updateUI(self, ui_states):
+        '''Changes ui elements depending on ui_states'''
+        print("signal update ui recived")
+
+        for ui_state in ui_states:
+            if(ui_state in self.ui_elements_refnames):
+                state = ui_states.get(ui_state)
+                widget = self.ui_elements_refnames.get(ui_state)
+
+                if(isinstance(state, bool)):
+                    widget.setEnabled(state)
+
+                if(isinstance(widget, QPlainTextEdit)):
+                    widget.appendPlainText(state)
+
+            else:
+                print("error : ui element referenced by non existing name")
--- modbus_tcp/utils/connectivity.py
+++ modbus_tcp/utils/connectivity.py
@@ -2,12 +2,20 @@ import socket
 import threading
 import time
 
-class Modbus():
+from PySide2.QtCore import QObject
+from PySide2.QtCore import Signal
+
+class Communication(QObject):
+    updateUI = Signal(dict)
+
+class Modbus(QObject):
     def __init__(self, coupler_ip, port):
         self.coupler_ip = coupler_ip
         self.port = int(port)
         self.data_to_send = None
         self.running = True
+
+        self.communication = Communication()
 
     def start(self):
         thread = threading.Thread(target=self.run)
@@ -19,15 +27,15 @@ class Modbus():
 
         try:
             # connecting
-            print("connecting to " + self.coupler_ip + ":" + str(self.port))
+            self.communication.updateUI.emit({'output' : "connecting to " + self.coupler_ip + ":" + str(self.port)})
             socket_tcp.connect((self.coupler_ip, self.port), )
         except socket.error as e:
-            # could not connect
-            print("could not connect")
+            self.communication.updateUI.emit({'output', 'could not connect'})
             return
 
         # update UI
-        print("connected to "  + self.coupler_ip + ":" + str(self.port))
+        self.communication.updateUI.emit({'relay_1' : True, 'relay_2' : True, 'relay_3' : True, 'relay_4' : True})
+        self.communication.updateUI.emit({'output' : "connected to "  + self.coupler_ip + ":" + str(self.port)})
 
         while self.running:
             time.sleep(1)
@@ -36,7 +44,7 @@ class Modbus():
                 try:
                     socket_tcp.send(self.data_to_send)
                 except socket.error as e:
-                    print("Error")
+                    self.communication.updateUI.emit({'output' : 'Error'})
                     self.data_to_send = None
 
                     if (e.errno == socket.errno.ECONNABORTED):
